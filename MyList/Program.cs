56b299647fca53d1e3f1d915928940dc3ad718d4
Samet-Cihangir <samet.cihangir@outlook.com>
2021-01-28 17:26:29 +0300

    Add project files.

---
+++ MyList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Samet");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Cihangir");
+
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
